05937a4ae6b52055f6ed1acee92083d5183d4d4a
alex <Mrsuper1221@gmail.com>
2024-10-18 22:41:11 +0300

    feat: working AI with 74%

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+from argparse import ArgumentError
+
+import numpy as np
 import torch
 import tqdm
+from PIL import Image
+from matplotlib import pyplot as plt
 from torch import nn
 from torch.utils.data import DataLoader
 
@@ -8,7 +14,22 @@ from dataset.dataset import AnimalDataset
 from dataset.load import load_labels
 import torchvision.transforms as tt
 
+
 from sklearn.model_selection import train_test_split
+
+def dump_to_cvs(file_names, predictions):
+    if len(file_names) != len(predictions):
+        raise ArgumentError(argument=None, message="predictions length does not match file names length")
+    with open("result/result.csv", "w") as file:
+        file.write("image_name,predicted_class\n")
+        for i in range(len(file_names)):
+            file.write(f"{file_names[i]},{predictions[i]}\n")
+
+
+classes = {
+    0: "Заяц", 1: "Кабан", 2: "Кошки", 3: "Куньи", 4: "Медведь", 5: "Оленевые", 6: "Пантеры", 7: "Полорогие",
+    8: "Собачие", 9: 'Сурок'
+}
 
 labels = load_labels("train.csv")
 
@@ -56,6 +77,8 @@ def train(dataloader, model, loss_fn, optimizer):
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
+
+
 """
         if batch % 5 == 0:
             loss, current = loss.item(), (batch + 1) * len(X)
@@ -63,6 +86,7 @@ def train(dataloader, model, loss_fn, optimizer):
 
 best_val_f1 = 0.0
 best_model_path = 'best_model.pth'
+
 
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
@@ -91,13 +115,48 @@ def test(dataloader, model, loss_fn):
     print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
+IS_LEARNING = False
 
-epochs = 15
-for t in range(epochs):
-    print(f"Epoch {t + 1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
+if IS_LEARNING:
+    epochs = 15
+    for t in range(epochs):
+        print(f"Epoch {t + 1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer)
+        test(test_dataloader, model, loss_fn)
+    print("Done!")
+else:
+    model.load_state_dict(torch.load("best_model.pth", weights_only=True))
 
+infer_transform = tt.Compose([
+    tt.Resize((224, 224)),
+    tt.ToTensor(),
+    tt.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 
+with torch.no_grad():
+    model.eval()
+    _, _, file_names = list(os.walk("test"))[0]
+    res = []
 
-print("Done!")
+    for file_name in tqdm.tqdm(file_names):
+        full_path = "test" + "/" + file_name
+        image = Image.open(full_path).convert('RGB')
+        image = infer_transform(image)
+        image.unsqueeze_(0)
+        image = image.to(device)
+        prediction = model(image)
+        res.append(torch.argmax(model(image), dim=1).cpu().numpy()[0])
+
+    plt.figure(figsize=(25, 25))
+    for i in range(25):
+        file_name = file_names[i]
+        full_path = "test" + "/" + file_name
+        image = Image.open(full_path).convert('RGB')
+        ax = plt.subplot(5, 5, i + 1)
+        plt.imshow(image)
+        plt.title(classes[res[i]])
+        plt.axis("off")
+
+    plt.show()
+
+    dump_to_cvs(file_names, res)
